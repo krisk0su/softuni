7596b4769456265ff30d3102bb068445bae1b825
krisk0su <chrizkoof@gmail.com>
2017-12-17 17:25:37 +0200

    a

---
+++ OOP/FamilyMembers/Person.cs
@@ -0,0 +1,26 @@
+﻿
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person()
+        {
+            this.name = "No name";
+            this.age = 1;
+
+        }
+
+        public Person(int age)
+        {
+            this.name = "No name";
+            this.age = age;
+        }
+
+        public Person(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+    }
+
