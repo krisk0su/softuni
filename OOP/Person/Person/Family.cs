a147b465f602ad04408c05f5f2f797811127f074
krisk0su <chrizkoof@gmail.com>
2017-12-17 17:21:39 +0200

    new

---
+++ OOP/Person/Person/Family.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Family
+    {
+        public List<Person> listOfPeople;
+
+
+        public Family()
+        {
+            this.listOfPeople = new List<Person>();
+        }
+
+        public void AddMember(Person person)
+        {
+          this.listOfPeople.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            var oldest = this.listOfPeople.Select(x => x.age).Max();
+            var index = this.listOfPeople.FindIndex(x => x.age == oldest);
+            return this.listOfPeople[index];
+        } 
+    }
+
